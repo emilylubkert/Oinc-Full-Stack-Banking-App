64caf56be234677aa21df2e49df7bb948724b014
emilylubkert <elubkert@gmail.com>
2022-04-05 13:51:35 -0600

    update file structure

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static(path.join(__dirname, './frontend/build')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
+    res.sendFile(path.join(__dirname, './frontend/build', 'index.html'))
 })
 
 try {
