c5120058efb9c00e80a095a94cfd68e07b8529b1
emilylubkert <elubkert@gmail.com>
2022-04-05 13:38:09 -0600

    update server

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../frontend/build')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build'))
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
 })
 
 try {
