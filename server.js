51f26a1fb3b424c32e7c922acc015e88f1a4bdb0
emilylubkert <elubkert@gmail.com>
2022-04-05 21:06:41 -0600

    update path

--- server.js
+++ server.js
@@ -23,9 +23,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, './frontend/build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/build', 'index.html'))
-})
 
 try {
   mongoose
@@ -134,6 +131,10 @@ app.post('/transactions/withdraw', async (req, res) => {
     console.log(error);
   }
 });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './frontend/build', 'index.html'))
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
