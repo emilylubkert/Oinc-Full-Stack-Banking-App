1b9cb658a21c5b99ef3b9f0c6259823c7f852792
emilylubkert <elubkert@gmail.com>
2022-04-05 20:22:07 -0600

    fix typo in path

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, './frontend/build')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/build', 'index.html'))
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
 })
 
 try {
