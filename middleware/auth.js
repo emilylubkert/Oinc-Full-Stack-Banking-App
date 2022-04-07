1bc69c909ecf15f8f364a0bb733c8b7b9ddfc5f0
emilylubkert <elubkert@gmail.com>
2022-04-07 10:30:31 -0600

    troubleshoot > update middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
 const firebase = require('../config/firebase')
 
 const authMiddleware = async (req, res, next) => {
-    if(!req.headers.authorization) {
-        res.status(403).json({message: 'No token provided'})
-    }
+    // if(!req.headers.authorization) {
+    //     // res.status(403).json({message: 'No token provided'})
+    // }
     const token = req.headers.authorization?.split(' ')[1];
     try {
         const decodedValue = await firebase.auth().verifyIdToken(token)
